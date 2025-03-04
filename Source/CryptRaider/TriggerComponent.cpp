174c1a856818ac0213fb5d201f0a112babd5f6f9
yeomin <jun1778@naver.com>
2025-03-04 21:50:04 +0900

    103: Index based & Range based For Loops

--- Source/CryptRaider/TriggerComponent.cpp
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -25,9 +25,17 @@ void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 	TArray<AActor*> Actors;
 	GetOverlappingActors(Actors);
 
+	// index base for문 
 	for (int i=0; i < Actors.Num(); i++)
 	{
 		FString ActorName = Actors[i]->GetActorNameOrLabel();
+		UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
+	}
+
+	// 포인터를 사용해서 Range Base For문 
+	for (AActor* Actor: Actors)
+	{
+		FString ActorName = Actor->GetActorNameOrLabel();
 		UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
 	}
 
