e94a999ddf143fb1074f1fcf624f788a298fcccf
yeomin <jun1778@naver.com>
2025-03-04 21:59:28 +0900

    104: Actor Tags
    - Actor Tag를 사용하여 내가 원하는 컴포넌트만 선택할 수 있다.

--- Source/CryptRaider/TriggerComponent.cpp
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -24,19 +24,15 @@ void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 
 	TArray<AActor*> Actors;
 	GetOverlappingActors(Actors);
-
-	// index base for문 
-	for (int i=0; i < Actors.Num(); i++)
-	{
-		FString ActorName = Actors[i]->GetActorNameOrLabel();
-		UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
-	}
-
+	
 	// 포인터를 사용해서 Range Base For문 
 	for (AActor* Actor: Actors)
 	{
-		FString ActorName = Actor->GetActorNameOrLabel();
-		UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
+		//  Actor의 태그가 맞는지 확인
+		if (Actor->ActorHasTag(AcceptableActorTag))
+		{
+			UE_LOG(LogTemp, Display, TEXT("Overlapping"));
+		}
 	}
 
 }
