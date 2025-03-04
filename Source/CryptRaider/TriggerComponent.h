d1419ebfd451f1d288070629bec0f951244ec618
yeomin <jun1778@naver.com>
2025-03-04 20:27:41 +0900

    99: Overlap Events
    Block, Overlap, Ignore 설정에 따라서 Pawn과 Obkect별 충돌 설정을 다르게 조절할 수 있다

---
+++ Source/CryptRaider/TriggerComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "TriggerComponent.generated.h"
+
+/**
+ * UCLASS 안에 meta 데이터가 있어야 블루프린트 안에서 불러올 수 있다!
+ */
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CRYPTRAIDER_API UTriggerComponent : public UBoxComponent
+{
+	GENERATED_BODY()
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+};
